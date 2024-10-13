1b7eebf02054a7f45fa07e442b81ba9e7c2e56ae
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-13 13:45:38 +0200

    Created list of employee objects and pushed each object into a stack

---
+++ OOPGenericCollections/Helper.cs
@@ -0,0 +1,6 @@
+﻿namespace OOPGenericCollections;
+
+public class Helper
+{
+    
+}
--- OOPGenericCollections/Program.cs
+++ OOPGenericCollections/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Employee> listOfEmployees =
+        [
+            new Employee("101", "Jake", Employee.EmployeeGender.Male, 50000),
+            new Employee("102", "Joice", Employee.EmployeeGender.Female, 40000),
+            new Employee("103", "Jasper", Employee.EmployeeGender.Male, 30000),
+            new Employee("104", "Jennifer", Employee.EmployeeGender.Female, 20000),
+            new Employee("105", "John", Employee.EmployeeGender.Male, 500000)
+        ];
+
+        Stack<Employee> stackOfEmployees = Helper<Employee>.PushToStack(listOfEmployees);
         
+        foreach (var employee in stackOfEmployees)
+        {
+            Console.WriteLine(employee.Name);
+        }
     }
 }
