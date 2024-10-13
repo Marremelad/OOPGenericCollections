f2f2381d082919e1c21d15e9d0c86d2882d24597
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-13 14:45:26 +0200

    Finished first part of assignment

--- OOPGenericCollections/Program.cs
+++ OOPGenericCollections/Program.cs
@@ -13,11 +13,43 @@ class Program
             new Employee("105", "John", Employee.EmployeeGender.Male, 500000)
         ];
 
-        Stack<Employee> stackOfEmployees = Helper<Employee>.PushToStack(listOfEmployees);
+        Stack<Employee> stackOfEmployees = new Stack<Employee>(listOfEmployees);
         
         foreach (var employee in stackOfEmployees)
         {
-            Console.WriteLine(employee.Name);
+            Console.WriteLine($"items left in the stack = {stackOfEmployees.Count}");
+            Console.WriteLine($"{employee.Id} - {employee.Name} - {employee.Gender} -  {employee.Salary}");
+        }
+        Console.WriteLine("------------------------------");
+        
+        Console.WriteLine("Retrieve Using Pop Method");
+        while (stackOfEmployees.Count > 1)
+        {
+            var employee = stackOfEmployees.Pop();
+            Console.WriteLine($"{employee.Id} - {employee.Name} - {employee.Gender} -  {employee.Salary}");
+            Console.WriteLine($"Items left in the stack = {stackOfEmployees.Count}");
+        }
+        Console.WriteLine("------------------------------");
+
+
+        // stackOfEmployees = Helper<Employee>.PushToStack(listOfEmployees); // Use this line to have all employees in the stack.
+        
+        stackOfEmployees.Push(listOfEmployees[0]); // Use these three lines to only have three of the five employees in the stack. 
+        stackOfEmployees.Push(listOfEmployees[1]);
+        stackOfEmployees.Push(listOfEmployees[2]);
+        
+        Console.WriteLine("Retrieve Using Peek Method");
+        for (int i = 0; i < 2; i++)
+        {
+            var employee = stackOfEmployees.Peek();
+            Console.WriteLine($"{employee.Id} - {employee.Name} - {employee.Gender} -  {employee.Salary}");
+            Console.WriteLine($"Items left in the stack = {stackOfEmployees.Count}");
+        }
+        Console.WriteLine("------------------------------");
+
+        for (int i = 0; i < listOfEmployees.Count; i++)
+        {
+            Console.WriteLine(stackOfEmployees.Contains(listOfEmployees[i]) ? $"Emp{i + 1} is in the stack" : $"Emp{i + 1} is not in the stack" );
         }
     }
 }
