c402b32eeea327057571f791312cc77c22032e57
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-13 12:35:45 +0200

    First commit

---
+++ OOPGenericCollections/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace OOPGenericCollections;
+
+public class Employee
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public EmployeeGender Gender { get; set; }
+    public decimal Salary { get; set; }
+
+    public enum EmployeeGender
+    {
+        Male,
+        Female,
+        Other,
+    }
+
+    public Employee(string id, string name, EmployeeGender gender, decimal salary)
+    {
+        Id = id;
+        Name = name;
+        Gender = gender;
+        Salary = salary;
+    }
+    
+    
+}
---
+++ OOPGenericCollections/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace OOPGenericCollections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
